0acd567919265cf00e73e0618b20b109e5ec1112
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-10 16:59:48 -0500

    commentation

--- hangman.js
+++ hangman.js
@@ -1,11 +1,18 @@
+//Inquirer NPM required
 var inquirer = require('inquirer');
 
+//Word progress, score count, guess count
 var progress = '';
 var score = 0;
 var guesses = 15;
+
+//Track where user correctly guessed letters in a word
 var correctIndices = [];
+
+//Generate random word
 var randomWord = Word();
 
+//Inquirer prompt object
 var question = {
     type: 'input',
     message: 'Guess a letter!',
@@ -13,47 +20,62 @@ var question = {
     default: 'a'
 }
 
-//Prompt
+//Receive user input, then...(promise)
 inquirer.prompt([question]).then(response => {
-  Game(response);
+    //Invoke Game() with user response
+    Game(response);
 });
 
+//Game: Takes an inquirer response
 function Game(response) {
-    var response = new Letter(response.letter, randomWord);
+    //Creates a new letter object with response.letter and the current randomWord (for comparison)
+    response = new Letter(response.letter, randomWord);
+    //Progress gets the result of response.check(), which returns an updated string with the user's guess factored in
     progress = response.check();
-    if (!randomWord.includes(response.letter)) guesses--;
+
+    //If user's letter guess (response.letter) is not in the randomWord, decrement guesses
+    if (!randomWord.includes(response.letter.toUpperCase())) guesses--;
+
+    //Log guessesRemaining and user's progress
     console.log('Guesses remaining: ', guesses);
     console.log(progress);
+
+    //If the progress string has no more underscores, increment score and generate new randomWord
     if (!progress.includes('_')) {
         score++;
+        guesses = 15;
         correctIndices = [];
         randomWord = Word();
         console.log('Great job! Current score:', score);
         console.log('Next word...');
     }
+    //Else if user has no more guesses, decrement score and generate new random word
     else if (guesses < 1) {
         score--;
-        console.log("Out of guesses :( the word was", randomWord);
-        console.log('Current score:', score);
+        guesses = 15;
+        correctIndices = [];
+        randomWord = Word();
+        console.log('Out of guesses :( Current score:', score);
         console.log('Next word...');
     }
+
+    //Prompt user for next letter guess
     inquirer.prompt(question).then(response => {
         Game(response);
     });
 }
 
-//Word:  Generates a random word
+//Word: Generates and returns a random word
 function Word() {
     var wordArray = ['LIZARD', 'BEAR', 'SQUID', 'CAMEL', 'TURTLE', 'FOX', 'OSTRICH', 'PLATYPUS', 'CROCODILE', 'KANGAROO', 'BANDICOOT'];
     var randomIndex = Math.floor(Math.random() * wordArray.length);
     return wordArray[randomIndex];
 }
 
-//Letter:  Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data.
+//Letter: Takes in a letter and a word. Check method compares letter to all letters within the word
 function Letter(letter, word) {
     this.letter = letter;
     this.word   = word;
-    
     //Check() method: iterates through word and checks if it contains the letter guessed.
     this.check  = () => {
         var splitWord = this.word.split('');
@@ -68,6 +90,7 @@ function Letter(letter, word) {
                 splitWord[i] = ' _ ';
             }
         }
+        //Return updated string
         this.word = splitWord.join('');
         return this.word;
     }
